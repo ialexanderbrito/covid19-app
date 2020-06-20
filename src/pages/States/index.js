42a90dbfbdd8f4a8e9576ae28d59bf7ecd27127d
Alexander <xanderzinho26@gmail.com>
2020-06-20 15:22:59 -0300

    Finalizado página de Estados

--- src/pages/States/index.js
+++ src/pages/States/index.js
@@ -1,11 +1,93 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 
-import { Wrapper, Title } from './styles';
+import {
+  Wrapper,
+  Container,
+  Title,
+  GoBack,
+  Navbar,
+  Logo,
+  Description,
+  SearchBar,
+  Search,
+  List,
+  ListItem,
+  ListTitle,
+} from './styles';
 
-export default function Global() {
+import logo from '../../assets/logo.png';
+
+export default States = () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const { states } = route.params;
+
+  const [search, setSearch] = useState('');
+  const [filter, setFilter] = useState(states);
+
+  useEffect(() => {
+    const result = states.data.filter((state) =>
+      state.state.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilter(result);
+  }, [search]);
+
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
+
+  navigateToDetails = (state) => {
+    navigation.navigate('DetailStates', { detail: state });
+  };
+
   return (
     <Wrapper>
-      <Title>Covid 19</Title>
+      <Container>
+        <Navbar>
+          <GoBack onPress={handleNavigateBack}>
+            <AntDesign name="arrowleft" size={24} color="#004dd5" />
+          </GoBack>
+          <Title>Covid 19</Title>
+          <Logo source={logo} resizeMode="contain" />
+        </Navbar>
+        <Title>Situação nos Estados</Title>
+        <Description>
+          O Corona vírus está se espalhando cada vez mais nos países. Faça a sua
+          parte e ajude o mundo a vencer esse vírus.
+        </Description>
+
+        <SearchBar>
+          <Search
+            value={search}
+            onChangeText={setSearch}
+            placeholder="Procure um estado..."
+          />
+          <Ionicons name="ios-search" size={24} color="#004dd5" />
+        </SearchBar>
+
+        <List
+          data={filter}
+          keyExtractor={(states) => String(states.data)}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item: state }) => (
+            <ListItem
+              onPress={() => {
+                navigateToDetails(state);
+              }}
+            >
+              <ListTitle>{state.state}</ListTitle>
+              <Ionicons
+                name="ios-arrow-round-forward"
+                size={32}
+                color="#004dd5"
+              />
+            </ListItem>
+          )}
+        />
+      </Container>
     </Wrapper>
   );
-}
+};
--- src/pages/States/styles.js
+++ src/pages/States/styles.js
@@ -1,11 +1,79 @@
 import styled from 'styled-components/native';
-import Constants from 'expo-constants';
 
-export const Wrapper = styled.ScrollView`
+export const Wrapper = styled.SafeAreaView`
   flex: 1;
   background: #ebebeb;
 `;
 
+export const Container = styled.ScrollView`
+  padding: 30px 24px;
+`;
+
+export const Navbar = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const GoBack = styled.TouchableOpacity``;
+
 export const Title = styled.Text`
-  font-size: 16px;
+  font-size: 24px;
+  font-family: ${'Montserrat_800ExtraBold'};
+  color: #004dd5;
+`;
+
+export const Logo = styled.Image`
+  width: 50px;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: 24px;
+  font-family: ${'Montserrat_800ExtraBold'};
+  color: #fff;
+  margin-bottom: 10px;
+`;
+
+export const Description = styled.Text`
+  margin-top: 10px;
+  font-size: 14px;
+  font-family: ${'Roboto_400Regular'};
+  line-height: 20px;
+  color: #666;
+`;
+
+export const SearchBar = styled.View`
+  background: #fff;
+  margin-bottom: 40px;
+  padding: 20px;
+  border-radius: 8px;
+  margin-top: 40px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Search = styled.TextInput`
+  width: 220px;
+  color: #1b1b1b;
+  font-family: ${'Roboto_500Medium'};
+`;
+
+export const List = styled.FlatList`
+  margin-bottom: 100px;
+`;
+
+export const ListItem = styled.TouchableOpacity`
+  background: #fff;
+  padding: 20px 20px;
+  border-radius: 15px;
+  margin-bottom: 20px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ListTitle = styled.Text`
+  font-family: ${'Roboto_500Medium'};
+  color: #1b1b1b;
 `;

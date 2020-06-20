16572f6797a2f49d680b613c905b8048c8473510
Alexander <xanderzinho26@gmail.com>
2020-06-20 11:55:21 -0300

    Adicionado página dos Estados

---
+++ src/pages/States/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Wrapper, Title } from './styles';
+
+export default function Global() {
+  return (
+    <Wrapper>
+      <Title>Covid 19</Title>
+    </Wrapper>
+  );
+}

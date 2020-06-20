11d057d4e3ab7fa765482062db551e3b9814551b
Alexander <xanderzinho26@gmail.com>
2020-06-20 00:46:00 -0300

    Concluido header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import { FontAwesome5 } from '@expo/vector-icons';
 
-import { Wrapper, Title } from './styles';
+import {
+  Wrapper,
+  Container,
+  Navbar,
+  Title,
+  Logo,
+  Header,
+  SubTitle,
+  Description,
+  Situation,
+  SituationButton,
+  SituationText,
+} from './styles';
+
+import logo from '../../assets/logo.png';
 
 export default function Home() {
   return (
     <Wrapper>
-      <Title>Covid 19</Title>
+      <Container>
+        <Navbar>
+          <Title>Covid 19</Title>
+          <Logo source={logo} resizeMode="contain" />
+        </Navbar>
+        <Header>
+          <SubTitle>O que é Corona Vírus?</SubTitle>
+          <Description>
+            O coronavírus (COVID-19) é uma doença infecciosa causada por um novo
+            vírus.
+          </Description>
+          <Description>
+            Ele causa problemas respiratórios semelhantes à gripe e sintomas
+            como tosse, febre e, em casos mais graves, dificuldade para
+            respirar.
+          </Description>
+          <Situation>
+            <SituationButton style={{ backgroundColor: '#FBBD09' }}>
+              <FontAwesome5 name="globe-americas" size={20} color="#fff" />
+              <SituationText>Brasil</SituationText>
+            </SituationButton>
+
+            <SituationButton>
+              <FontAwesome5 name="flag" size={20} color="#fff" />
+              <SituationText>Estados</SituationText>
+            </SituationButton>
+          </Situation>
+        </Header>
+      </Container>
     </Wrapper>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,11 +1,72 @@
 import styled from 'styled-components/native';
 import Constants from 'expo-constants';
 
-export const Wrapper = styled.ScrollView`
+export const Wrapper = styled.SafeAreaView`
   flex: 1;
   background: #ebebeb;
 `;
 
+export const Container = styled.ScrollView`
+  padding: 30px 24px;
+`;
+
+export const Navbar = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 export const Title = styled.Text`
+  font-size: 24px;
+  font-family: ${'Montserrat_800ExtraBold'};
+  color: #004dd5;
+`;
+
+export const Logo = styled.Image`
+  width: 50px;
+`;
+
+export const Header = styled.View`
+  margin-top: 20px;
+  padding: 30px 20px;
+  background-color: #fff;
+  border-radius: 8px;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: 24px;
+  font-family: ${'Montserrat_800ExtraBold'};
+  color: #004dd5;
+  margin-bottom: 10px;
+`;
+
+export const Description = styled.Text`
+  font-size: 14px;
+  font-family: ${'Roboto_400Regular'};
+  line-height: 20px;
+  color: #666;
+`;
+
+export const Situation = styled.TouchableOpacity`
+  margin-top: 40px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const SituationButton = styled.TouchableOpacity`
+  height: 55px;
+  width: 150px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background: #33b540;
+  border-radius: 10px;
+`;
+
+export const SituationText = styled.Text`
+  color: #fff;
+  font-family: ${'Roboto_500Medium'};
+  font-weight: bold;
   font-size: 16px;
+  margin-left: 10px;
 `;

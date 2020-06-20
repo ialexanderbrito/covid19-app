d0c1a2a31b73a69e3ac89c683f4a0b332a033c7b
Alexander <xanderzinho26@gmail.com>
2020-06-20 00:45:31 -0300

    Adicionado fonts

--- App.js
+++ App.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { AppLoading } from 'expo';
+
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import {
+  Montserrat_800ExtraBold,
+  useFonts,
+} from '@expo-google-fonts/montserrat';
 
 import Routes from './src/routes';
 
 export default function App() {
-  return <Routes />;
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Montserrat_800ExtraBold,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  return (
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Routes />
+    </>
+  );
 }
